8ba6fa7f70261ba942842e261940dd1f4bf55acd
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-03 19:56:00 +0530

    adding the answer

--- restaurant-app/src/App.jsx
+++ restaurant-app/src/App.jsx
@@ -1,1 +1,26 @@
+import {Routes,Route,Navigate} from "react-router-dom";
 
+export default function App(){
+    return(
+        <Rotues>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/admin/dashboard" element={<ProtectedRoute role="admin">
+                <AdminDashboard/>
+            </ProtectedRoute>}/>
+
+            <Route path="/admin/restaurant/add" element= {<ProtectedRoute role="admin">
+                <AddRestaurant/>
+            </ProtectedRoute>}/>
+
+            <Rotues path="/admin/restaurants/update/:id" element={<ProtectedRoute role="admin">
+                <UpdatedRestauartant/>
+            </ProtectedRoute>}/>
+
+            <Route path="/customer/dashboard" element={<ProtectedRoute role="customer">
+                <CustomerDashboard/>
+            </ProtectedRoute>}/>
+
+            <Route path="*" element={<Navigate to="/login"/>}/>
+        </Rotues>
+    );
+}
